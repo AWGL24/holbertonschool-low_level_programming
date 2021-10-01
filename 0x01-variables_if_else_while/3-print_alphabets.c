07fbd661e7e2afb5ea8fd423dbcea4b7de1e2b81
Drew-24 <3239@holbertonschool.com>
2021-10-01 18:38:49 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int A;
+
+	for (a = 97; a <= 122; a++)
+		putchar(a);
+	
+	for (A = 65; A <= 90; A++)
+		putchar(A);
+
+	putchar ('\n');
+	return (0);
+}
