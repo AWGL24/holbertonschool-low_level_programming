c5c48b054d2479a6eace1fecb720ccf8ef284cca
Drew-24 <3239@holbertonschool.com>
2021-10-12 17:50:16 +0000

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - function that prints every other character
+ * @str: pointer
+ */
+void puts2(char *str)
+{
+	long unsigned int i;
+
+	for (i = 0; i < strlen(str); i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
