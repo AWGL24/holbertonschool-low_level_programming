56ee37a389990aa83fe6849a625ced00f3d312c5
Drew-24 <3239@holbertonschool.com>
2021-10-14 15:32:35 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strncpy - function that copies a string
+ * @dest: 1st string
+ * @src: 2nd string
+ * @n: parameter
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	if ((dest == NULL) && (src == NULL))
+		return (NULL);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
