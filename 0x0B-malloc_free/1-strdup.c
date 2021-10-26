a10fe6b0543648a678ec0f6a8af160d1962557c3
Drew-24 <3239@holbertonschool.com>
2021-10-26 13:55:59 +0000

    The woman who has no imaginarion has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strdup - function that returns a pointer
+ * to a newly allocated space in memory
+ * @str: parameter
+ * Return: duplicate
+ */
+char *_strdup(char *str)
+{
+	int str_size;
+	static char *dup;
+	char *dup_offset;
+
+	str_size = strlen(str);
+	dup = (char *)malloc(sizeof(char) * str_size + 1);
+	if (dup == NULL)
+		return ( (char *) NULL);
+
+	dup_offset = dup;
+	while (*str)
+	{
+		*dup_offset = *str;
+		dup_offset++;
+		str++;
+	}
+	*dup_offset = '\0';
+
+	return (dup);
+}
