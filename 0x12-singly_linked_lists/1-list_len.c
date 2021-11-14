80a1c516049c0421ca7f5b87f911774a032ca208
Drew-24 <3239@holbertonschool.com>
2021-11-14 00:33:05 +0000

    List lenght

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - function that returns the len of a list
+ * @h: head of the list
+ * Return: lenght
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
