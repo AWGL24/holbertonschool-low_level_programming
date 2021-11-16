904be6baabe846a90a793436f69779501fd90f2f
Drew-24 <3239@holbertonschool.com>
2021-11-16 18:46:26 +0000

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *free_list - function that frees a list_t list
+ *free_listint2 - function that frees a list_t list
  *@head: list
  */
 void free_listint2(listint_t **head)
