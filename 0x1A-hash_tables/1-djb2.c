42ccc6b608a91aa82356af22df6df43e5c37dd0f
Drew-24 <andrewwil1@hotmail.com>
2022-01-27 17:28:34 +0000

    djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,9 @@
+#include "hash_tables.h"
+/**
+ * hash_dbj2 - dbj2 algorithm
+ * @param str
+ * @return unsigned long int
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
