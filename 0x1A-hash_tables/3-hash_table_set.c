15b0da36dfc67f4af0516c1de47615812e5d1a2e
Andrew Gonzalez <andrewwil1@hotmail.com>
2022-01-28 13:58:41 -0400

    fixed code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,34 +8,34 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node = NULL, *tmp;
-	unsigned long int idx = 0;
+	unsigned long int hash;
+	hash_node_t *new = NULL, *tmp = NULL;
 
-	if (key == NULL || value == NULL || ht == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 		return (0);
-
-	node->key = (char *)key;
-	node->value = (char *)value;
-	idx = key_index((const unsigned char *)key, ht->size);
-
-	if (ht->array[idx] == NULL)
+	new->key = (char *)key;
+	new->value = (char *)value;
+	new->next = NULL;
+	hash = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[hash] == NULL)
 	{
-		ht->array[idx] = node;
+		ht->array[hash] = new;
+		return (1);
 	}
 	else
 	{
-		tmp = ht->array[idx];
+		tmp = ht->array[hash];
 		while (tmp != NULL)
 		{
 			tmp = tmp->next;
 		}
-		tmp = ht->array[idx];
-		node->next = tmp;
-		ht->array[idx] = node;
+		tmp = ht->array[hash];
+		new->next = tmp;
+		ht->array[hash] = new;
+		return (1);
 	}
-	return (1);
 }
