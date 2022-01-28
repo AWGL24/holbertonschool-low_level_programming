ecf3a3962cae059b37dbd36c29ea858319983e86
Andrew Gonzalez <andrewwil1@hotmail.com>
2022-01-28 13:17:26 -0400

    Fixed betty

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,18 +1,18 @@
 #include "hash_tables.h"
 /**
- * hash_dbj2 - dbj2 algorithm
- * @param str
- * @return unsigned long int
+ * hash_dbj2 - function that applies dbj2 algorithm
+ * @str: parameter
+ * Return: unsigned long int
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
