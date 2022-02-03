406524122bd3115441de901e612f4720dcc741aa
Drew-24 <andrewwil1@hotmail.com>
2022-02-03 14:41:03 -0400

    Fixed code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,10 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 	unsigned long int i = 0;
 
-	table = malloc(sizeof(hash_node_t));
-	if (table == NULL)
+	if (table = NULL)
 		return (NULL);
 
 	table->array = malloc(sizeof(hash_node_t *) * size);
