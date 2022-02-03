3c684b7afee78c6fd65dcc89a455aeb42cd1a3ab
Drew-24 <andrewwil1@hotmail.com>
2022-02-03 14:51:30 -0400

    Fixed code

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,24 +5,22 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i;
+	unsigned long int i = 0;
 	hash_node_t *node;
 
-	if (ht == NULL || ht->array == NULL || !ht->size)
+	if (ht == NULL)
 		return;
-	for (i = 0; i < ht->size; i++)
+	for (; i <= ht->size; i++)
 	{
-		while (ht->array[i])
+		node = ht->array[i];
+		while (node != NULL)
 		{
-			node = ht->array[i]->next;
-			free(ht->array[i]->key);
-			free(ht->array[i]->value);
-			free(ht->array[i]);
-			ht->array[i] = node;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = node->next;
 		}
 	}
 	free(ht->array);
-	ht->array = NULL;
-	ht->size = 0;
 	free(ht);
 }
